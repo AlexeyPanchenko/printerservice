69bc2cd813cde30a898570168f891dd8b4d38d7e
ViktorJasch <viktory9494@mail.ru>
2017-12-12 17:16:36 +1000

    fix the one page error add PrintAdapter subclass for writing from byteArray

--- printerservice/src/main/java/com/example/print/printerserver/PrintService.kt
+++ printerservice/src/main/java/com/example/print/printerserver/PrintService.kt
@@ -3,6 +3,9 @@ package com.example.print.printerserver
 import android.content.Context
 import android.print.PrintManager
 import android.support.v7.app.AppCompatActivity
+import com.example.print.printerserver.adapters.ByteArrayPrintAdapter
+import com.example.print.printerserver.adapters.FilePrintAdapter
+import com.example.print.printerserver.adapters.StreamPrintAdapter
 import java.io.File
 import java.io.InputStream
 
@@ -10,6 +13,7 @@ import java.io.InputStream
 interface IPrintService {
     fun print(inputStream: InputStream, name: String = "Document")
     fun print(file: File, name: String = "Document")
+    fun print(b: ByteArray, name: String = "Document")
 }
 
 class PrintService(private val context: Context) : IPrintService {
@@ -20,6 +24,10 @@ class PrintService(private val context: Context) : IPrintService {
 
     override fun print(file: File, name: String) {
         startPrint(name, FilePrintAdapter(file))
+    }
+
+    override fun print(b: ByteArray, name: String) {
+        startPrint(name, ByteArrayPrintAdapter(b))
     }
 
     private fun startPrint(name: String, adapter: PrintAdapter) {
--- printerservice/src/main/java/com/example/print/printerserver/adapters/StreamPrintAdapter.kt
+++ printerservice/src/main/java/com/example/print/printerserver/adapters/StreamPrintAdapter.kt
@@ -1,5 +1,7 @@
-package com.example.print.printerserver
+package com.example.print.printerserver.adapters
 
+import com.example.print.printerserver.PrintAdapter
+import com.example.print.printerserver.write
 import java.io.FileDescriptor
 import java.io.FileOutputStream
 import java.io.InputStream
